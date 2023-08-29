a1b3dbfef7ed34a03fd4082323221c93382d675f
samuelboada21 <samuelomarboba@ufps.edu.co>
2023-08-29 12:58:05 -0500

    port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 import {connectDB} from './db.js';
 
+const port = 3000;
+
 connectDB(); 
-app.listen(3000);
-console.log('Server on port', 3000);
+app.listen(port);
+console.log('Server on port', port);
