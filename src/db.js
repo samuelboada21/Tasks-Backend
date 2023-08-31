17a0b6f809f82ec2e33ff25a7a9aab49bbc00ba8
samuelboada21 <samuelomarboba@ufps.edu.co>
2023-08-31 11:06:41 -0500

    DataBase MongoDB atlas

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1/merndb");
+    await mongoose.connect("mongodb+srv://samuelomarboba:MongoDB2121@cluster0.jm7bcn8.mongodb.net/?retryWrites=true&w=majority");
     console.log("DB is Connected");
   } catch (error) {
     console.log(error);
